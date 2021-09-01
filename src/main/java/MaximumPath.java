6c92b15e86fb0eec550055c1be27c869662c73da
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-02 00:43:20 +0100

    refactor

--- src/main/java/MaximumPath.java
+++ src/main/java/MaximumPath.java
@@ -1,6 +1,13 @@
 public class MaximumPath {
     public static void main(String[] args) {
-        int x = 2+2;
-        System.out.println(x);
+        int[][] triangle = {{3}, {7,4},{2,4,6}, {8,5,9,3}};
+        int sum = triangle[0][0];
+        int j = 0;
+        for (int i=1; i<triangle.length; i++) {
+            int left = triangle[i][j];
+            int right = triangle[i][j++];
+            sum += Math.max(left, right);
+        }
+        System.out.println(sum);
     }
 }
