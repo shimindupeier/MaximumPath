eb927e5e368b87912e34c3840b834b747a967064
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-09-01 16:02:21 +0100

    initial commit

---
+++ src/main/java/MaximumPath.java
@@ -0,0 +1,6 @@
+public class MaximumPath {
+    public static void main(String[] args) {
+        int x = 1+2;
+        System.out.println(x);
+    }
+}
---
+++ src/test/java/MaximumPathTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumPathTest {
+
+    @Test
+    public void testAssertions() {
+        assertEquals(2, 1+1);
+    }
+}
